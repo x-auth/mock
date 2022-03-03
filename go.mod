a28f5edfb96915e2cf592a4a8f22d1a8117e4fce
nl <nl@x-net.at>
2022-03-03 11:38:42 +0100

    Hotfixes

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/x-auth/mock
 
 go 1.15
 
-require github.com/x-auth/common v0.0.0-20220204072731-2381665a1b5e // indirect
+require github.com/x-auth/common v0.0.0-20220216170134-e062fda2a33d // indirect
--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
-import "github.com/x-auth/common/models"
+import (
+	"github.com/x-auth/common/models"
+	"github.com/x-auth/common/plugins"
+)
 
 type Plugin struct {
 	username string
 	password string
 }
 
-func (p *plugin) Login(username string, password string) (models.Profile, bool) {
+func (p *Plugin) Login(username string, password string) (models.Profile, bool) {
 	if username == p.username && password == p.password {
 		return models.Profile{
 			Name:        "Foo Bar",
@@ -18,13 +21,12 @@ func (p *plugin) Login(username string, password string) (models.Profile, bool) 
 			PhoneNumber: "000000000",
 		}, true
 	}
-
 	return models.Profile{}, false
 }
 
-func Init(username string, password string) Plugin {
-	return Plugin{
-		username: username,
-		password: password,
+func NewPlugin(cfg map[string]string) plugins.AuthPlugin {
+	return &Plugin{
+		username: cfg["username"],
+		password: cfg["password"],
 	}
 }
